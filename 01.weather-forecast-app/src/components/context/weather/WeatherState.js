c29d1e9a2cbdf34748ab522751c915fd96684fa2
Yavor Petkov <hermsues@gmail.com>
2021-08-21 16:45:54 +0300

    context added

--- 01.weather-forecast-app/src/components/context/weather/WeatherState.js
+++ 01.weather-forecast-app/src/components/context/weather/WeatherState.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import weatherContext from './weatherContext';
+import weatherReducer from './weatherReducer';
 
-const WeatherState = () => {};
+const WeatherState = (props) => {
+	const initialState = {};
+
+	const [ state, dispatch ] = useReducer(weatherReducer, initialState);
+
+	return <weatherContext.Provider value={{}}>{props.children}</weatherContext.Provider>;
+};
 
 export default WeatherState;
