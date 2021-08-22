c198723337c9c7c6597f7c4f574ff8f55c38a2c2
Yavor Petkov <hermsues@gmail.com>
2021-08-22 17:43:37 +0300

    axios get weather data

--- 01.weather-forecast-app/src/components/context/weather/WeatherState.js
+++ 01.weather-forecast-app/src/components/context/weather/WeatherState.js
@@ -1,14 +1,33 @@
 import React, { useReducer } from 'react';
-
+import axios from 'axios';
 import weatherContext from './weatherContext';
 import weatherReducer from './weatherReducer';
 
 const WeatherState = (props) => {
-	const initialState = {};
+	const initialState = {
+		city: '',
+		tempC: []
+	};
 
 	const [ state, dispatch ] = useReducer(weatherReducer, initialState);
 
-	return <weatherContext.Provider value={{}}>{props.children}</weatherContext.Provider>;
+	// GET WEATHER
+	const getWeather = async () => {
+		const res = await axios.get(
+			`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=Sofia&aqi=no`
+		);
+		console.log(res.data);
+	};
+
+	return (
+		<weatherContext.Provider
+			value={{
+				getWeather
+			}}
+		>
+			{props.children}
+		</weatherContext.Provider>
+	);
 };
 
 export default WeatherState;
